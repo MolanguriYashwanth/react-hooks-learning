3c07afb0776f6ab9133cc8c4b8545a714ba274ff
smolangu <smolangu@SMOLANGU-IN.in.oracle.com>
2019-10-08 19:08:36 +0530

    Added Custom Http Hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,25 +1,27 @@
-import React, { useState,useReducer ,useCallback,useMemo} from 'react';
+import React, { useState,useEffect, useReducer, useCallback, useMemo } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
+import useHttp from '../../hooks/http';
 
-const ingredientReducer = (currentIngredients,action)=>{
-  switch(action.type){
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
     case "SET":
       return action.ingredients;
     case "ADD":
-      return [...currentIngredients, action.ingredient.ops[0]]  
+      return [...currentIngredients, action.ingredient.ops[0]]
     case "DELETE":
-      return  currentIngredients.filter((item) => item.id !== action.id) 
-      default:
-        throw new Error("Shouldnt reach here");
+      return currentIngredients.filter((item) => item.id !== action.id)
+    default:
+      throw new Error("Shouldnt reach here");
   }
 }
 
 const Ingredients = () => {
-  const [userIngredients,dispatch] =useReducer(ingredientReducer,[])
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
   // useEffect(() => {
   //   fetch('http://localhost:8085/my/ingredients/')
   //     .then(response => response.json())
@@ -28,61 +30,106 @@ const Ingredients = () => {
   //     })
   // }, [])
   //const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading,setLoading]=useState(false);
-  const [error,setError]=useState();
+  // const [isLoading,setLoading]=useState(false);
+  // const [error,setError]=useState();
+
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifer,
+    clear
+  } = useHttp();
+
+  useEffect(() => {
+    if (!isLoading && !error && reqIdentifer === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifer === 'ADD_INGREDIENT') {
+      dispatch({
+        type: 'ADD',
+        ingredient: data
+      });
+    }
+  }, [data, reqExtra, reqIdentifer, isLoading, error]);
+
+  // const addIngredientHandler = useCallback(ingredient => {
+  //   setLoading(true);
+  //   fetch("http://localhost:8085/my/ingredients/data",
+  //     {
+  //       method: 'POST',
+  //       body: JSON.stringify(ingredient),
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     }).then((response) => {
+  //       setLoading(false);
+  //       return response.json();
+  //     }).then(responseData => {
+  //       // setUserIngredients(prevIngredients =>
+  //       //   [...prevIngredients, responseData.ops[0]])
+  //       dispatch({ type: "ADD", ingredient: responseData })
+  //     }
+  //     )
+
+  // }, [])
+
   const addIngredientHandler = useCallback(ingredient => {
-    setLoading(true);
-    fetch("http://localhost:8085/my/ingredients/data",
-      {
-        method: 'POST',
-        body: JSON.stringify(ingredient),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then((response) => {
-        setLoading(false);
-        return response.json();
-      }).then(responseData => {
-        // setUserIngredients(prevIngredients =>
-        //   [...prevIngredients, responseData.ops[0]])
-        dispatch({type:"ADD",ingredient:responseData})
-      }
-      )
+    sendRequest(
+      'http://localhost:8085/my/ingredients/data',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    );
+  }, [sendRequest]);
+  
+  // const removeIngredientHandler = useCallback((title, id) => {
+  //   //const filteredData = userIngredients.filter((item) => item.id !== id);
+  //   setLoading(true);
+  //   fetch(`http://localhost:8085/my/ingredients/delteingrdient?title=${title}`, {
+  //     method: 'DELETE'
+  //   }).then(response => {
+  //     setLoading(false);
+  //     //setUserIngredients(prevIngredients => prevIngredients.filter((item) => item.id !== id))
+  //     dispatch({ type: "DELETE", id: id })
+  //   }).catch(error => {
+  //     setError(error.message);
+  //     setLoading(false)
+  //   })
 
-  },[])
-  const removeIngredientHandler = useCallback((title,id) => {
-    //const filteredData = userIngredients.filter((item) => item.id !== id);
-    setLoading(true);
-    fetch(`http://localhost:8085/my/ingredients/delteingrdient?title=${title}`,{
-      method: 'DELETE'
-    }).then(response=>{
-      setLoading(false);
-      //setUserIngredients(prevIngredients => prevIngredients.filter((item) => item.id !== id))
-      dispatch({type:"DELETE",id:id})
-    }).catch(error=>{
-      setError(error.message);
-      setLoading(false)
-    })
+  // }, [])
 
-  },[])
+  const removeIngredientHandler = useCallback(
+    (title, id) => {
+      sendRequest(
+        `http://localhost:8085/my/ingredients/delteingrdient?title=${title}`,
+        'DELETE',
+        null,
+        id,
+        'REMOVE_INGREDIENT'
+      );
+    },
+    [sendRequest]
+  );
+
   const onLoadingIngredientsHandler = useCallback(filteredData => {
     //setUserIngredients(filteredData)
-    dispatch({type:"SET",ingredients:filteredData})
-  },[]);
+    dispatch({ type: "SET", ingredients: filteredData })
+  }, []);
 
-  const onErrorHandler = useCallback(() =>{
-    setError(null);
-  },[])
-  const ingredientList = useMemo(()=>{
+
+  const ingredientList = useMemo(() => {
     return (
       <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
 
     )
-  },[userIngredients,removeIngredientHandler])
+  }, [userIngredients, removeIngredientHandler])
   return (
     <div className="App">
-      {error && <ErrorModal onClose={onErrorHandler}>{error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loadingIndicator={isLoading}/>
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loadingIndicator={isLoading} />
 
       <section>
         <Search onLoadingIngredients={onLoadingIngredientsHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -2,11 +2,15 @@ import React, { useState,useEffect ,useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
+import ErrorModal from '../UI/ErrorModal';
+import useHttp from '../../hooks/http';
 
 const Search = React.memo(props => {
   const {onLoadingIngredients} = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef();
+  const { isLoading, data, error, sendRequest, clear } = useHttp();
+
   useEffect(()=>{
     const timer =setTimeout(()=>{
       if(enteredFilter===inputRef.current.value){
@@ -16,23 +20,35 @@ const Search = React.memo(props => {
         }else{
           url='http://localhost:8085/my/ingredients/'
         }
-        fetch(url)
-        .then(response=>response.json())
-        .then(responseData=>{
-          onLoadingIngredients(responseData)
-        })
+        // fetch(url)
+        // .then(response=>response.json())
+        // .then(responseData=>{
+        //   onLoadingIngredients(responseData)
+        // })
+        sendRequest(
+          url,
+          'GET'
+        );
       }
     },500)
     return () => {
       clearTimeout(timer);
     }
   
-  },[enteredFilter,onLoadingIngredients,inputRef])
+  },[enteredFilter,sendRequest,inputRef])
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      onLoadingIngredients(data);
+    }
+  }, [data, isLoading, error, onLoadingIngredients]);
   return (
     <section className="search">
+            {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input type="text"
           ref={inputRef}
            value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
